e7e7353314797886535164285e0e16a6f42eb56d
Erika De La Luz <11618062+delaluzEri@users.noreply.github.com>
2020-07-02 18:02:15 -0500

    Links y conversiones

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -9,7 +9,14 @@
                   class="font-semibold text-xl tracking-tight">PlatziExchange</router-link>
             </div>
             <div class="hidden sm:block w-full block flrex-grow lg:flex lg:items-center lg:w-auto">
-               <div class="text-sm lg:flex-grow"></div>
+               <div class="text-sm lg:flex-grow">
+                  <router-link
+                     v-for="l in links" :key="l.title"
+                     :to="l.to"
+                     class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                     {{l.title}}
+                  </router-link>
+               </div>
             </div>
          </nav>
       </nav>
@@ -20,7 +27,13 @@
 import PxIcon from '@/components/PxIcon';
 export default {
    name: 'PxHeader',
-   components: { PxIcon }
+   components: { PxIcon },
 
+   props: {
+      links: {
+         type: Array,
+         default: () => []
+      }
+   }
 }
 </script>
